1bd882833077e29b07730912c8b58e8aa5391036
sora <jinny3770@github.com>
2016-03-17 15:32:01 +0900

    add actionbar

---
+++ Coins2/app/src/main/java/com/example/sora/coins/ChatActivity.java
@@ -0,0 +1,11 @@
+package com.example.sora.coins;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sora on 2016-03-17.
+ */
+public class ChatActivity extends AppCompatActivity {
+
+
+}
--- Coins2/app/src/main/java/com/example/sora/coins/MainActivity.java
+++ Coins2/app/src/main/java/com/example/sora/coins/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.sora.coins;
 
-import android.app.ActionBar;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import net.daum.mf.map.api.*;
 
@@ -18,14 +26,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xFFFF8080));
+        actionBar.setTitle("");
+        //actionBar.setIcon(R.drawable.chat);
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        LayoutInflater actionBarInflater = LayoutInflater.from(this);
+
+        View customView = actionBarInflater.inflate(R.layout.actionbar_layout, null);
+
+        //ImageButton chatButton = (ImageButton) customView.findViewById(R.id.menuButton);
+
+        actionBar.setCustomView(customView);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        //mapView = (MapView)findViewById(R.id.mapView);
         mapLayout = (LinearLayout) findViewById(R.id.mapView);
 
         mapView = new MapView(this);
-        mapView.setDaumMapApiKey("API");
-        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
-
+        mapView.setDaumMapApiKey(APIKey.ApiKey);
+        //mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
         mapLayout.addView(mapView);
 
 
+
+        mapView.setShowCurrentLocationMarker(true);
+
+    }
+
+    public boolean onCreateOptionMenu(Menu menu){
+
+        return true;
     }
 }
